121dd15e3c4292190a02793849b8023092e9aef6
Santiago Developer <Santiago Developer@DESKTOP-S2D4PQE.fibertel.com.ar>
2023-05-26 08:34:34 -0300

    Ejercicio n#1 de Actividades afianciamiento.  Mi primer "Hola Mundo" en Java. :)

---
+++ src/main/java/com/mycompany/miprimerprograma/MiPrimerPrograma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miprimerprograma;
+
+/**
+ *
+ * @author Santiago Developer
+ */
+public class MiPrimerPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
